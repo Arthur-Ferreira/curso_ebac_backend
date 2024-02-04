a729d74294df351c8f17f3b82db54ae2e467fc52
Arthur-Ferreira <arthurfdarosa@gmail.com>
2024-02-04 15:37:21 -0300

    Calculation of Average exercise upload

---
+++ 7_VariaveisTipos&Operadores/MediaDeNotas/src/Main.java
@@ -0,0 +1,13 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+  public static void main(String[] args) {
+    int nota1 = 7;
+    int nota2 = 7;
+    int nota3 = 2;
+    int nota4 = 4;
+    int media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+    System.out.println(media);
+  }
+}
