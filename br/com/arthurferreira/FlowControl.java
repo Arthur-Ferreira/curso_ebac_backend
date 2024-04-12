cf9863a80ebc749c84ec8b587a2d442a4a0f8cbb
Arthur Ferreira <arthurfdarosa@gmail.com>
2024-04-12 11:26:13 -0300

    Project set up

---
+++ br/com/arthurferreira/FlowControl.java
@@ -0,0 +1,15 @@
+package br.com.arthurferreira;
+
+public class FlowControl {
+
+     public static void main(String[] args) {
+         // Variables
+
+
+         // Input
+
+         // Logic
+
+         // Output
+     }
+}
