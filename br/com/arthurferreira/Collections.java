4746a0e50daa7022a48ea1ba423faad0aeaa1884
Arthur Ferreira <arthurfdarosa@gmail.com>
2024-05-17 11:29:25 -0300

    Project structure

---
+++ br/com/arthurferreira/Collections.java
@@ -0,0 +1,18 @@
+package br.com.arthurferreira;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Collections {
+  public static void main(String[] args) {
+    // Scanner declaration
+    Scanner scan = new Scanner(System.in);
+
+    // Arrays declarations
+    List<String> maleGroup = new ArrayList<>();
+    List<String> femaleGroup = new ArrayList<>();
+
+
+  }
+}
