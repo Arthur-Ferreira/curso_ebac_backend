c3812d57d339b788503b9152527462f539ab1165
Arthur Ferreira <arthurfdarosa@gmail.com>
2024-05-17 11:55:13 -0300

    Project finished

--- br/com/arthurferreira/Collections.java
+++ br/com/arthurferreira/Collections.java
@@ -13,6 +13,34 @@ public class Collections {
     List<String> maleGroup = new ArrayList<>();
     List<String> femaleGroup = new ArrayList<>();
 
+    // Logic
 
+    while (true) {
+      System.out.print("Digite o nome: ");
+      String nome = scan.nextLine();
+
+      System.out.print("Digite o sexo (M/F): ");
+      String sexo = scan.nextLine().toUpperCase();
+
+      if(sexo.equals("M")) {
+        maleGroup.add(nome);
+      } else if (sexo.equals("F")) {
+        femaleGroup.add(nome);
+      } else {
+        System.out.println("Sexo inválido. Por favor, digite uma opção válida.");
+      }
+
+      System.out.print("Deseja continuar (S/N)? ");
+      String continuar = scan.nextLine().toUpperCase();
+
+      if (continuar.equals("N")) {
+        break;
+      }
+    }
+
+    System.out.println("Grupo Masculino: " + maleGroup);
+    System.out.println("Grupo Feminino: " + femaleGroup);
+
+    scan.close();
   }
 }
