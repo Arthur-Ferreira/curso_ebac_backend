297423b08f3108801ea30d5230774931b770e976
Arthur Ferreira <arthurfdarosa@gmail.com>
2024-05-23 16:13:27 -0300

    Feat Project 1: Client getters and setters update

---
+++ br/com/arthurferreira/dao/IClienteDAO.java
@@ -0,0 +1,19 @@
+package br.com.arthurferreira.dao;
+
+import br.com.arthurferreira.domain.Cliente;
+
+import java.util.Collection;
+import java.util.List;
+
+public interface IClienteDAO {
+    
+    public Boolean cadastrar(Cliente cliente);
+    
+    public void excluir(Long cpf);
+    
+    public void alterar(Cliente cliente);
+    
+    public Cliente consultar(Long cpf);
+
+    public Collection<Cliente> buscarTodos();
+}
