51695bdb261cac16f038d5eb36e6dc9bbca60e41
Arthur Ferreira <arthurfdarosa@gmail.com>
2024-06-06 00:18:47 -0300

    Feat Steam: Remove unused import

--- br/com/arthurferreira/App.java
+++ br/com/arthurferreira/App.java
@@ -2,7 +2,6 @@ package br.com.arthurferreira;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class App {
   public static void main(String[] args) {
